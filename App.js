2f44169e538a1d25bce8a15aa28dd2e783bdaa69
KomalVyas23 <77508110+KomalVyas23@users.noreply.github.com>
2023-02-25 19:00:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+ import React from 'react';
+ import ReactDOM from 'react-dom/client';
+
+        const heading1 = React.createElement("h1",{
+            id:'heading1'
+        },"Heading 1!");
+
+        const heading2 = React.createElement("h2",{
+            id:'heading2'
+        }, 'Heading 2!');
+
+        const container = React.createElement("div",{
+            className: 'conatiner'
+        }, [heading1, heading2]);
+
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(container);
